0b4fa651666d604c58310c8f815d0e54b8be4319
Low Jian Sheng <lowjiansheng@hotmail.sg>
2016-10-08 15:30:14 +0800

    Add false test for isTask method

--- src/test/java/seedu/address/parser/AddParserTest.java
+++ src/test/java/seedu/address/parser/AddParserTest.java
@@ -18,12 +18,22 @@ public class AddParserTest {
 	}
 	
 	@Test
+	public void checkIsTask_returnsFalse(){
+		
+		String arguments = "by: 10-08-16 9999";
+		
+		assertFalse(AddParser.isTask(arguments));
+	}
+	
+	
+	@Test
 	public void checkIsEvent_returnsTrue(){
 		
 		String arguments = "from: 10-08-2016 1900 to: 11-08-2016 1900";
 		
 		assertTrue(AddParser.isEvents(arguments));
 	}
+	
 	
 	@Test
 	public void checkIsFloatingTask_returnsTrue(){
