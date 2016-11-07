c460053d1a86774df8aad6fced58d5670bd1f2cf
Brehmer <Brehmerchan@gmail.com>
2016-11-07 19:24:19 +0800

    fix bug with task sorting

--- src/main/java/seedu/menion/model/TaskComparator.java
+++ src/main/java/seedu/menion/model/TaskComparator.java
@@ -22,34 +22,47 @@ public class TaskComparator implements Comparator<Activity> {
     public int compare(Activity activityA, Activity activityB) {
         
         //sort by completion status 
-        
-        if (compareByCompletionStatus(activityA, activityB) != 0){
+        if (compareCompletedStatus(activityA, activityB) != 0){
             return completeSortInt;
         }
         //if both tasks have same completion status we sort by start date
         else {
-          //compares by start date
-            if (compareByDate(activityA, activityB) != 0) {
-                return compareByDate(activityA, activityB);
-            }
             
-            //if both events have same state date, we sort by start time
+            
+            if (compareActivityStartDate(activityA, activityB) != 0){
+                return dateSortInt;
+            }
+            //if both tasks have same state date, we sort by start time
             else {
-                return compareByTime(activityA, activityB);
+                compareActivityStartTime(activityA, activityB);
+                
+                return timeSortInt;
             }
         }
     }
-    
-    /**
-     * This method compares two activities by their completion status
-     * @param activityA is the first activity
-     * @param activityB is the second activity
-     * @return returns 0 if both have same completion status
-     *          returns 1 if activityA is completed and activityB is uncompleted
-     *          returns -1 if activityB is completed and activityA is uncompleted
-     */
-    private int compareByCompletionStatus(Activity activityA, Activity activityB) {
-        int completeSortInt;
+
+    private void compareActivityStartTime(Activity activityA, Activity activityB) {
+        time1 = activityA.getActivityStartTime().toString();
+        time2 = activityB.getActivityStartTime().toString();
+                   
+        timeSortInt = time1.compareTo(time2);
+    }
+
+    private int compareActivityStartDate(Activity activityA, Activity activityB) {
+        date1 = activityA.getActivityStartDate().toString();
+        date2 = activityB.getActivityStartDate().toString();
+        
+        String[] valueOfDate1 = date1.split("-");
+        String[] valueOfDate2 = date2.split("-");
+        
+        date1 = valueOfDate1[2] + valueOfDate1[1] + valueOfDate1[0];
+        date2 = valueOfDate2[2] + valueOfDate2[1] + valueOfDate2[0];
+        
+        dateSortInt = date1.compareTo(date2);
+        return dateSortInt;
+    }
+
+    private int compareCompletedStatus(Activity activityA, Activity activityB) {
         if (activityA.getActivityStatus().toString().equals(activityB.getActivityStatus().toString())) {
             completeSortInt = 0;
         }
@@ -61,41 +74,5 @@ public class TaskComparator implements Comparator<Activity> {
             completeSortInt = -1;
         }
         return completeSortInt;
-    }
-    
-    /**
-     * This method compares two activities by their starting time as strings
-     * @param activityA is the first activity
-     * @param activityB is the second activity
-     * @return the the two start time lexicographically
-     */
-    private int compareByTime(Activity activityA, Activity activityB) {
-        time1 = activityA.getActivityStartTime().toString();
-        time2 = activityB.getActivityStartTime().toString();
-
-        timeSortInt = time1.compareTo(time2);
-
-        return timeSortInt;
-    }
-
-    /**
-     * This method compares two activities by their starting date as strings
-     * @param activityA is the first activity
-     * @param activityB is the second activity
-     * @return the the two start dates lexicographically
-     */
-    private int compareByDate(Activity activityA, Activity activityB) {
-        date1 = activityA.getActivityStartDate().toString();
-        date2 = activityB.getActivityStartDate().toString();
-
-        String[] valueOfDate1 = date1.split("-");
-        String[] valueOfDate2 = date2.split("-");
-
-        date1 = valueOfDate1[2] + valueOfDate1[1] + valueOfDate1[0];
-        date2 = valueOfDate2[2] + valueOfDate2[1] + valueOfDate2[0];
-
-        dateSortInt = date1.compareTo(date2);
-        
-        return dateSortInt;
     }
 }
